fe4ee0b53dfbc2686bf1c8eae50442026b0029d3
Kyle Muldoon <kylemuldoon15@gmail.com>
2023-05-26 16:17:29 -0700

    set up game board and bg-tiles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react'
 
 const styles = {
   app: `
@@ -15,25 +15,27 @@ const styles = {
     text-3xl 
     font-bold
   `,
-  tile: `
+  bg_tile: `
     flex 
     flex-col 
     justify-center 
-    border-4 
-    rounded-md
+    rounded
     items-center 
-    text-9xl
-    w-60 
-    h-60 
+    text-5xl
+    bg-gray-700
+    flex-grow
+    aspect-square
   `,
   header: `
     flex 
     flex-row 
-    text-9xl 
     w-full 
     bg-gray-600 
     justify-center 
-    items-center`
+    items-center
+    p-2
+    text-4xl
+    `
 };
 
 
@@ -41,22 +43,7 @@ export default function App(): ReactElement {
   return (
     <div className={styles.app + styles.text}>
       <Header />
-      <GameBoard />
-    </div>
-  )
-}
-
-type TileProps = Readonly<{
-  value: number,
-}>
-
-function Tile(props: TileProps): ReactElement {
-
-  const { value } = props;
-
-  return (
-    <div className={styles.tile}>
-      {value}
+      <GameBoardContainer />
     </div>
   )
 }
@@ -70,10 +57,55 @@ function Header(): ReactElement {
 }
 
 
-function GameBoard(): ReactElement {
+function GameBoardContainer(): ReactElement {
   return (
     <div className='h-full w-full flex flex-col items-center justify-center border-4 border-spacing-4'>
-      <Tile value={2} />
+      <GameBoard />
     </div>
   )
+}
+
+type TileProps = Readonly<{
+  value: number,
+}>
+
+function BGTile(props: TileProps): ReactElement {
+
+  const { value } = props;
+
+  return (
+    <div className={styles.bg_tile}>
+      {value}
+    </div>
+  )
+}
+
+function GameBoard(): ReactElement {
+
+  // const [board, updateBoard] = useState<number>(0);
+
+
+
+  return (
+    <div className='w-[600px] h-[600px] rounded bg-gray-300 flex flex-col justify-around p-2'>
+      <div className='grid grid-cols-4 gap-2 grid-rows-4 grid-'>
+        <BGTile value={1} />
+        <BGTile value={2} />
+        <BGTile value={3} />
+        <BGTile value={4} />
+        <BGTile value={5} />
+        <BGTile value={6} />
+        <BGTile value={7} />
+        <BGTile value={8} />
+        <BGTile value={9} />
+        <BGTile value={10} />
+        <BGTile value={11} />
+        <BGTile value={12} />
+        <BGTile value={13} />
+        <BGTile value={14} />
+        <BGTile value={15} />
+        <BGTile value={16} />
+      </div>
+    </div>
+  );
 }
