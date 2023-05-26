ad9dff3d7722cca63e43112990c8546e43f6e73a
Kyle Muldoon <kylemuldoon15@gmail.com>
2023-05-25 22:51:21 -0700

    Set up vite, tailwind, ts, and get base app component set up

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react'
+
+export default function App(): ReactElement {
+  return (
+    <h1 className="text-3xl font-bold underline p-40">
+      Hello world!
+    </h1>
+  )
+}
